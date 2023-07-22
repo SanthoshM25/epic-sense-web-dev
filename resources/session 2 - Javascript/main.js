337e7bc6c4e647291ef0531ac130870dc28b7568
santhoshm25 <santhoshm2504@gmail.com>
2023-07-22 21:56:37 +0530

    session 1 and 2 contents

---
+++ resources/session 2 - Javascript/main.js
@@ -0,0 +1,46 @@
+// var name = "Santhosh";
+// name = "Virat";
+// let age = 25;
+// const gender = "male";
+
+// var sayName = () => {
+//     console.log(name, "is " + age + " old");
+// }
+
+// var newSayName = sayName
+
+// newSayName()
+// sayName()
+
+// var header = document.getElementById("hdr");
+// console.log(header);
+
+//querySelector - select a component with its id, class or tag name.
+// var anotherHeader = document.querySelectorAll(".hdr");
+// console.log(anotherHeader);
+
+// var btn = document.getElementById("btn");
+
+// btn.addEventListener("click", function () {
+//   if (btn.style.backgroundColor === "red") {
+//     btn.style.backgroundColor = "green";
+//   } else {
+//     btn.style.backgroundColor = "red";
+//   }
+// });
+
+// let textInput = document.querySelector(".textInput");
+// let textPreview = document.querySelector(".textPreview");
+
+// let handleTextInput = (e) => {
+//   textPreview.textContent = e.target.value;
+// };
+
+// textInput.addEventListener("change", (e) => handleTextInput(e));
+
+
+//create a counter - should increase, decrease and reset values
+//create a todo - should add tasks to the list - https://todoista.netlify.app/
+//create a random name picker - should pick a random from a list of names - https://randomwordgenerator.com/name.php
+//create a color picker - should list a set of colors, when selected the background should change to that color
+//create a movie reviewer - list a set of movies, once clicked should display the review for it
